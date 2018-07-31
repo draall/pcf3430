abed2b00373f6178308c2377f41e17256b56ced9
Yong <draall@gmail.com>
2018-07-31 14:08:26 +0900

    수정 완료

--- pcf3430.py
+++ pcf3430.py
@@ -1,7 +1,7 @@
 import re
-from pandas import Series, DataFrame
-
 import os
+from pandas import DataFrame as df, ExcelWriter
+
 
 targetFiles = []
 listRefKey = []
@@ -18,7 +18,7 @@ for file in os.listdir(targerFolder):
     lenFileSplit = len(tempFileSpilt)
     tempFileEx = tempFileSpilt[lenFileSplit-1]
     
-    if bool(re.match('pcf', tempFileEx)):
+    if bool(re.match('pcf', tempFileEx, re.IGNORECASE)):
         targetFiles.append(os.path.join(targerFolder, file))
 
 for tagerFile in targetFiles:
@@ -32,34 +32,25 @@ for tagerFile in targetFiles:
         for line in lines:
             lineSplit = re.split('\s+', line)
             if lineSplit[0] == 'PIPELINE-REFERENCE':
-                pcfRefKey = lineSplit[0]
                 pcfRefVal = lineSplit[1]
                 iA = 1
             if lineSplit[1] == 'ATTRIBUTE30':
-                pcfAtt30Key = lineSplit[1]
                 pcfAtt30Val = lineSplit[2]
                 iB = 1
             if lineSplit[1] == 'ATTRIBUTE34':
-                pcfAtt34Key = lineSplit[1]
                 pcfAtt34Val = lineSplit[2]
                 iC = 1
             if iA == 1 and iB == 1 and iC == 1:
-                listRefKey.append(pcfRefKey)
                 listRefVal.append(pcfRefVal)
-                listAtt30Key.append(pcfAtt30Key)
                 listAtt30Val.append(pcfAtt30Val)
-                listAtt34Key.append(pcfAtt34Key)
                 listAtt34Val.append(pcfAtt34Val)
                 break
 
-            
-
-
-data = {'RefKey': listRefKey,
-        'RefVal': listRefVal,
-        'Att30Key': listAtt30Key,
-        'Att30Val': listAtt30Val,
-        'Att34Key': listAtt34Key,
-        'Att34Val': listAtt34Val}
-pcfDF = DataFrame(data)
+data = {'WBS ISO': listRefVal,          #input excel coulumn name
+        'ATTRIBUTE30': listAtt30Val,    #input excel coulumn name
+        'ATTRIBUTE34': listAtt34Val}    #input excel coulumn name
+pcfDF = df(data)
 pcfDF
+writer = ExcelWriter('PCF_output.xlsx')
+pcfDF.to_excel(writer,'PCF',index=False)
+writer.save()
